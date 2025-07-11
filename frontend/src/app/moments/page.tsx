56fd3c4f1ff791d75a09ac04ff8c511bf9d7a177
sderecat <spencer_derecat@intuit.com>
2025-07-11 10:37:01 -0700

    chore: adding placeholder for moments page

--- frontend/src/app/moments/page.tsx
+++ frontend/src/app/moments/page.tsx
@@ -7,6 +7,9 @@ export default function MomentsPage() {
     <>
       <div className="fixed inset-0 bg-pink-100 -z-10" />
       <Link href="/" className="fixed top-4 left-4 text-3xl text-black hover:underline cursor-pointer z-50">←</Link>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <span className="text-xl text-black text-center">still trying to figure out what to do with this page :)</span>
+      </div>
     </>
   );
 } 
