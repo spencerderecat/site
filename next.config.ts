602c76e1c6554ba22557b044405d1b40a45b724f
sderecat <spencer_derecat@intuit.com>
2025-07-11 14:24:12 -0700

    chore: dep

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   basePath: "/site",
+  assetPrefix: "/site",
 
   /**
    * Disable server-based image optimization. Next.js does not support
