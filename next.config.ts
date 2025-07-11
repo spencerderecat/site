b1c52379564c0480cb0b029b6eeb2fe584bcdfce
sderecat <spencer_derecat@intuit.com>
2025-07-11 14:45:22 -0700

    chore: fixed

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+    /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/site",
+  assetPrefix: "/site",
+
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
